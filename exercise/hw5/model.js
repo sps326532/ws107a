f6dfe122644afe96f5834d3f68314f2aab8f7dc3
z002020821 <z002020821@gmail.com>
2018-10-31 11:38:44 +0800

    server.js

---
+++ exercise/hw5/model.js
@@ -0,0 +1,21 @@
+const M = module.exports = {}
+
+const posts = []
+
+M.add = function (post) {
+  const id = posts.push(post) - 1
+  post.created_at = new Date()
+  post.id = id
+}
+
+M.modify = function (post) {
+  posts[post.id]=post
+}
+
+M.get = function (id) {
+  return posts[id]
+}
+
+M.list = function () {
+  return posts
+}
